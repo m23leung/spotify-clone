70f5246a600e1b7358df16539acfdedb1e92d3c6
Mark Leung <mark.leung89@gmail.com>
2020-10-10 20:34:03 +1100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [{user, token}, dispatch] = useDataLayerValue();
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    
+    if (_token) {
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token
+      })
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then( user => {
+        
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+
+      });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        })
+      });
+
+      spotify.getPlaylist('3QnrZtSgZ7bGuOUiyyw4Np').then( response => (
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      ));
+
+    }
+    console.log("I HAVE A TOKEN", _token);    
+  }, []);
+
+  console.log("USER IS ", user);
+  console.log("TOKEN: ", token)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    {
+      token ? (
+        <Player />
+      ) : (
+        <Login />
+      )
+    }
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,65 @@
+import { getTokenFromUrl } from "./spotify";
+
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    spotify: null,
+    discover_weekly: null,
+    top_artists: null
+    
+    //token: "BQCEXQtiCs-qSrm98hsG6NS5hjGk___Srqar6MYnWW8rLXLCF_492VjQd049gHdsEOBoCZGWm663Vp0rN2JEiTBcLInx7ixGgXc7AWGc-LFrglLcT6QYoCU9Pxa3rBaEZg2MR1dhCoPxDO1x_G//EyqZkAgI6ADRFcfVGFMJLKZhguSMpk7ZZD",
+}
+
+const reducer = (state, action) => { 
+    console.log(action);
+
+    switch(action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+        case 'SET_PLAYLISTS':
+            return {
+                ...state,
+                playlists: action.playlists
+            }      
+        case 'SET_DISCOVER_WEEKLY':
+            return {
+                ...state,
+                discover_weekly: action.discover_weekly
+            }   
+        case 'SET_PLAYING':
+            return {
+                ...state,
+                playing: action.playing
+            }
+        case "SET_ITEM":
+            return {
+                ...state,
+                item: action.item,
+            };    
+        case "SET_TOP_ARTISTS":
+            return {
+                ...state,
+                top_artists: action.top_artists,
+            };    
+        case "SET_SPOTIFY":
+        return {
+            ...state,
+            spotify: action.spotify,
+        };                                                               
+        default:
+            return state;
+    }
+
+}
+
+export default reducer;
